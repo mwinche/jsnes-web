4c1bb1c42350d1e0133cbd307d9710bbea6f30dc
Matt Winchester <mwinche@gmail.com>
2018-01-27 00:30:58 -0700

    allow for custom mappings

--- src/ControlsModal.jsx
+++ src/ControlsModal.jsx
@@ -8,6 +8,68 @@ import {
   Table
 } from "reactstrap";
 
+import { Controller } from "jsnes";
+
+import mappings, { KEYBOARD, gamepad } from './Mappings';
+
+const getGamepadButtonPress = () => new Promise(ok => {
+  let interval;
+  const checkForButton = () => Array.from(navigator.getGamepads())
+    .forEach((controller, gamepadIndex) => {
+      if(!controller){ return; }
+
+      const pressedIndex = controller.buttons.findIndex(({ pressed }) => pressed);
+
+      if(pressedIndex >= 0){
+        ok([gamepad(gamepadIndex), pressedIndex]);
+
+        if(typeof interval !== 'undefined'){
+          clearInterval(interval);
+        }
+      }
+    });
+
+  interval = setInterval(checkForButton, 16);
+});
+
+const getKeyPress = () => new Promise(ok => {
+  const preventDefault = event => event.preventDefault();
+
+  const handleKeyDown = event => {
+    ok([KEYBOARD, event.keyCode]);
+
+    event.preventDefault();
+
+    document.removeEventListener('keydown', handleKeyDown);
+    document.removeEventListener('keypress', preventDefault);
+  };
+
+  document.addEventListener('keydown', handleKeyDown);
+  document.addEventListener('keypress', preventDefault);
+});
+
+const handleClick = (player, button) => Promise.race([
+  getKeyPress(),
+  getGamepadButtonPress(),
+])
+  .then(data => { console.log(data); return data; })
+  .then(([source, key]) => mappings.addMapping(source, key, player, button));
+
+
+class Cell extends Component {
+  render(){
+    const { player, button } = this.props;
+
+    return <td
+      style={{ cursor: 'pointer' }}
+      onClick={() => handleClick(player, button).then(() => {
+        this.forceUpdate();
+      })}>
+      {mappings.getKeyFromButton(player, button)}
+    </td>;
+  }
+}
+
 class ControlsModal extends Component {
   render() {
     return (
@@ -29,43 +91,43 @@ class ControlsModal extends Component {
             <tbody>
               <tr>
                 <td>Left</td>
-                <td>Left</td>
-                <td>Num-4</td>
+                <Cell player={1} button={Controller.BUTTON_LEFT} />
+                <Cell player={2} button={Controller.BUTTON_LEFT} />
               </tr>
               <tr>
                 <td>Right</td>
-                <td>Right</td>
-                <td>Num-6</td>
+                <Cell player={1} button={Controller.BUTTON_RIGHT} />
+                <Cell player={2} button={Controller.BUTTON_RIGHT} />
               </tr>
               <tr>
                 <td>Up</td>
-                <td>Up</td>
-                <td>Num-8</td>
+                <Cell player={1} button={Controller.BUTTON_UP} />
+                <Cell player={2} button={Controller.BUTTON_UP} />
               </tr>
               <tr>
                 <td>Down</td>
-                <td>Down</td>
-                <td>Num-2</td>
+                <Cell player={1} button={Controller.BUTTON_DOWN} />
+                <Cell player={2} button={Controller.BUTTON_DOWN} />
               </tr>
               <tr>
                 <td>A</td>
-                <td>X</td>
-                <td>Num-7</td>
+                <Cell player={1} button={Controller.BUTTON_A} />
+                <Cell player={2} button={Controller.BUTTON_A} />
               </tr>
               <tr>
                 <td>B</td>
-                <td>Z</td>
-                <td>Num-9</td>
+                <Cell player={1} button={Controller.BUTTON_B} />
+                <Cell player={2} button={Controller.BUTTON_B} />
               </tr>
               <tr>
                 <td>Start</td>
-                <td>Enter</td>
-                <td>Num-1</td>
+                <Cell player={1} button={Controller.BUTTON_START} />
+                <Cell player={2} button={Controller.BUTTON_START} />
               </tr>
               <tr>
                 <td>Select</td>
-                <td>Ctrl</td>
-                <td>Num-3</td>
+                <Cell player={1} button={Controller.BUTTON_SELECT} />
+                <Cell player={2} button={Controller.BUTTON_SELECT} />
               </tr>
             </tbody>
           </Table>
